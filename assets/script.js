e4382424575ccef014657cddc08e91a92cf685ee
anddream <amargolis@berkeley.edu>
2016-11-29 03:56:56 -0800

    upload 3
    added hiding menu and resume zoom in

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
 
 
-$(window).scroll(function(){
-  	var currentScroll = $(window).scrollTop(); 
-  	//^this (scrollTop) caluclates how far you are from the top of the screen
-  if (currentScroll >= 300) {
-  		$("#nav").addClass("nav-bar-fixed");
-  }
- else { $("#nav").removeClass("nav-bar-fixed");}  
-  })
+$('#Layer_1').click(function(){
+	if ($('.navbar').hasClass('activate-nav')) {
+		$('.navbar').removeClass('activate-nav');
+		$('.window').removeClass('scoot');
+	} else {
+		$(".navbar").addClass('activate-nav');
+		$('.window').addClass('scoot');
+		}
+
 })
+
+$("#crop img").click(function() {
+ 	$(".resume img").fadeIn(1000);
+ 	$("#overlay").fadeIn(1000);
+});
+
+$("#overlay").click(function() {
+ 	$(".resume img").fadeOut(1000);
+ 	$("#overlay").fadeOut(1000);
+});
+
+
+});
